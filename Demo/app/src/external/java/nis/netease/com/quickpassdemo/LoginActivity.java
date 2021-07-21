ab9ce00f74f0f466b93bf61f299982488579f7b2
liulingfeng <liulingfeng@mistong.com>
2021-07-21 10:43:57 +0800

    feat：替换为最新3.0.7版本对应demo

--- Demo/app/src/external/java/nis/netease/com/quickpassdemo/LoginActivity.java
+++ Demo/app/src/external/java/nis/netease/com/quickpassdemo/LoginActivity.java
@@ -1,6 +1,7 @@
 package nis.netease.com.quickpassdemo;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -9,9 +10,6 @@ import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.netease.nis.quicklogin.QuickLogin;
 import com.netease.nis.quicklogin.helper.UnifyUiConfig;
 import com.netease.nis.quicklogin.listener.QuickLoginPreMobileListener;
